8ec5d4b3de984c53fb9ea7772c04047098734d03
Zhao Lei <1445089819@qq.com>
2023-12-15 18:07:54 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@ local opts = {
     noremap = true,
     silent = true
 }
-
 local term_opts = {
     silent = true
 }
@@ -17,17 +16,10 @@ keymap('n', 'J', '3j', opts)
 keymap('n', 'K', '3k', opts)
 keymap('n', 'L', '$', opts)
 keymap('n', 'H', '^', opts)
-keymap('n', '<leader>p', '\"*p', opts)
-keymap('n', '<leader>v', '<C-v>', opts)
-
--- use ctrl+v
-keymap('n', '<C-v>', '\"*p', opts)
-
+-- keymap('n', '<leader>p', '\"*p', opts)
+-- keymap('n', '<leader>v', '<C-v>', opts) ??
 -- when mouse double click a word replace them with '<C-v>', this use for copy from other place
-keymap('v', '<C-v>', 'd<Esc>\"*P', opts)
 
--- use ctrl-c for copy
-keymap('v', '<C-c>', '\"*y`>', opts)
 
 keymap('v', 'L', '$', opts)
 keymap('v', 'H', '^', opts)
@@ -35,16 +27,28 @@ keymap('v', 'K', '3k', opts)
 keymap('v', 'J', '3j', opts)
 
 -- When yanked, move the cursor to the end of the selected text
-keymap('v', '<leader>y', '\"*y`>', opts)
+-- keymap('v', '<leader>y', '\"*y`>', opts)
 keymap('x', 'y', 'y`>', opts)
-
+-- use ctrl+z
+keymap('n','<C-z>','u',opts)
 -- When pasted, move the cursor to the end of the yanked text
 keymap('n','p','p`]',opts)
+-- use ctrl+v for copy to system 
+keymap('n', '<C-v>', '\"*p', opts)
+-- use ctrl-v
+keymap('i', '<C-v>', '<Esc>\"*pa', opts)
+keymap('v', '<C-v>', 'd<Esc>\"*P', opts)
+
 if vim.g.vscode then
-    -- vim.api.nvim_exec([[
-    --     nnoremap <leader>c <Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>
-    --     ]], false)
+local vscode = require('vscode-neovim')
+-- vscode.call("bing-search.commandPaletteBingSearch")
+vim.keymap.set('n','<leader>s',function() vscode.call("bing-search.commandPaletteBingSearch") end,opts)
+vim.keymap.set('n','<leader>e',function() vscode.call("workbench.explorer.fileView.focus") end,opts)
 else
+
+-- use ctrl-c for copy to system
+keymap('v', '<C-c>', '\"*y`>', opts)
+
 vim.opt.number = true
 vim.opt.termguicolors = true
 vim.cmd[[colorscheme evening]]
